f162f8c98b89b350405035c6dd4122bdcd1622ef
DKFactotum <daniil.koshelyuk@gmail.com>
2020-07-12 23:09:07 +0200

    new structure

---
+++ Assets/Hololens/Scripts/UI/ToggleRadialView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+
+[RequireComponent(typeof(RadialView))]
+public class ToggleRadialView : MonoBehaviour {
+	private RadialView radialView;
+	// Start is called before the first frame update
+	void Start() {
+		this.radialView = GetComponent<RadialView>();
+	}
+    
+	public void OnToggleRadialView(){
+		this.radialView.enabled = !this.radialView.enabled;
+	}
+}
